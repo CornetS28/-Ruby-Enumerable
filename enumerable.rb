d690d497bb13917b210c8f0696d1e61b6b3de6f4
SamuelC28 <samuel@noukod.com>
2019-08-01 12:52:44 -0400

    my_select method created

--- enumerable.rb
+++ enumerable.rb
@@ -5,23 +5,33 @@ module Enumerable #module begins
         while  index < self.length #while loop begins
             yield(self[index])
              index+=1
-        end #while loop begins
+        end #while loop ends
        self
-    end #my_each method begins
+    end #my_each method end
 
     def my_each_with_index #my_each_with_index method begins
         index = 0
         while  index < self.length #while loop begins
             yield(self[index], index)
              index+=1
-        end #while loop begins
+        end #while loop ends
        self
     end #my_each_with_index  method begins
+
+    def my_select #my_select method begins
+        result_array = []
+        
+        self.my_each do |i| #iteration begins
+          yield(i) ? result_array.push(i) : ""
+         end #iteration begins
+         result_array
+      end #my_select method ends
 
 end #module ends
 
 
 
-# Testting our methods
+# Testing our methods
 [1,6,-3,8].my_each { |number| puts number-1 }
 [1,6,-3,8].my_each_with_index { |number, itemPosition| puts "#{itemPosition}: #{number*5}"}
+[1,6,-3,8].my_select { |number| puts number + 2}
